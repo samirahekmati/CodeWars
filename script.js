a30182bd6295e39da1b11d3c321a2dddb1c64031
samirahekmati <samirahekmati@gmail.com>
2024-11-22 09:54:54 +0000

    intalled jest testing framework

---
+++ script.js
@@ -0,0 +1,20 @@
+// function bonusTime(salary, bonus) {
+//   let total
+//  if ( bonus === true){
+//   total = salary * 10
+// } else {
+//   total = salary
+// }
+//   return `\u00A3${salary}`
+// }
+
+
+let a =["a","b","c"]
+let b=[]
+for(let i = 0; i < a.length; i++){
+  b[i]=a[i]
+}
+console.log(b)
+
+let c = Array.from(b)
+console.log(c)
