024756baf5702d2880f0327783a67351809da5ed
samirahekmati <samirahekmati@gmail.com>
2025-01-23 11:59:54 +0000

    map2

--- All things arrays/map.js
+++ All things arrays/map.js
@@ -2,7 +2,14 @@
 
 //In the following example, each number in an array is doubled.
 
-const numbers = [1,2,3,4]
-const newArray = numbers.map(element => element * 2);
-console.log("new array is double -->", newArray);
+const numbers1 = [1,2,3,4]
+// Pass a function to mapd
+const map1 = numbers1.map((element) => element * 2);
+console.log("doubled array -->", map1);
 
+
+//Example 2: Mapping an array of numbers to an array of square roots
+const numbers2 = [1,4,9];
+//pass function to map
+const map2 = numbers2.map((element) => Math.sqrt(element));
+console.log("square root -->", map2)
