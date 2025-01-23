862435284a37b9a659a3d8af2c23eae2a112e141
samirahekmati <samirahekmati@gmail.com>
2025-01-23 11:07:47 +0000

    litres.js

---
+++ Math/MathFloor.js
@@ -0,0 +1,8 @@
+//The Math.round() method rounds a number to the nearest integer.
+
+//The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+
+
+function litres(time) {
+    return Math.floor(time * 0.5)
+  }
