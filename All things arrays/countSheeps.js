31fdef5cd2664c4ec42994c31895dd409ee04330
samirahekmati <samirahekmati@gmail.com>
2025-01-23 11:04:09 +0000

    countsheep.js

---
+++ All things arrays/countSheeps.js
@@ -0,0 +1,31 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheep(array){
+    //retrun sheep.filter(Boolean).length
+    let sheepCount = 0;
+    for(let i = 0 ; i < array.length; i++){
+        if(array[i] === true){
+            sheepCount +=1
+        }
+    }
+    return sheepCount
+}
+
+console.log(countSheep([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]))
