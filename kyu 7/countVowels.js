978c9ba1954d454182dc954e1d6ac57261727078
samirahekmati <samirahekmati@gmail.com>
2025-01-24 12:03:54 +0000

    countVowels.js different solutions

--- kyu 7/countVowels.js
+++ kyu 7/countVowels.js
@@ -5,7 +5,7 @@
 // The input string will only consist of lower case letters and/or spaces.
 
 //solution with for loop
-function countVowels(str){
+function countVowelsWithForLoop(str){
     let count = 0;
     for(let i = 0; i < str.length ; i++){
         if(str[i] === "a" || str[i] === "e" || str[i] === "i" || str[i] === "o" || str[i] === "u") {
@@ -15,7 +15,27 @@ function countVowels(str){
     return count
 }
 
-console.log("count vowels with for loop -->", countVowels("Vowels Count Tests"))
+console.log("count vowels with for loop -->", countVowelsWithForLoop("Vowels Count Tests"))
 
 
-//solution with filter
+//another solution with for loop but this time different if statemen
+
+function countVowels(str){
+    let count=0;
+    for(let i = 0; i < str.length ; i++){
+        if("aeiouAEIOU".includes(str[i])){//check the current character in the loop
+            count++
+        }
+    }
+    return count
+}
+
+console.log("solution with for loop and include -->",countVowels("Vowels Count Tests"));
+
+//solution with filter and includes
+
+function countVowelsWithFilter (str) {
+    return str.split("").filter(i => "aeiouAEIOU".includes(i)).length
+}
+
+console.log("count vowels with filter and includes-->", countVowelsWithFilter("Vowels Count Tests"))
