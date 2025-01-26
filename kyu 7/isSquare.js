c12dc914af21452f6fa1d2d467ab646c6f6877cc
samirahekmati <samirahekmati@gmail.com>
2025-01-26 10:59:30 +0000

    isSquare.js

---
+++ kyu 7/isSquare.js
@@ -0,0 +1,14 @@
+// Task
+// Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+// Examples
+// -1  =>  false
+//  0  =>  true
+//  3  =>  false
+//  4  =>  true
+
+function isSquare(n){
+    
+}
