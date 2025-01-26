53413c13415b54eb1f57fd7c8834df1ee2281763
samirahekmati <samirahekmati@gmail.com>
2025-01-26 10:59:00 +0000

    middleChar.js

--- kyu 7/middleChar.js
+++ kyu 7/middleChar.js
@@ -16,3 +16,20 @@
 const string ="Hello Lilac"
 console.log(string.substring(6,11))
 console.log(string.substring(6))
+
+
+function getMiddle(str){
+    let position
+    let length
+    if(str.length % 2 == 1 ){
+        position = str.length / 2
+        length = 1;
+    }else {
+        position = str.length / 2-1
+        length = 2
+    }
+
+    return str.substring(position, position+length)
+}
+
+getMiddle("Lilac")
