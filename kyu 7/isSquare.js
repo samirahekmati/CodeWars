fe2fab16948bbf95add10b442e7aa2199e445f5b
samirahekmati <samirahekmati@gmail.com>
2025-02-21 14:55:36 +0000

    isSquare.js

--- kyu 7/isSquare.js
+++ kyu 7/isSquare.js
@@ -10,5 +10,13 @@
 //  4  =>  true
 
 function isSquare(n){
-    
+    if( (n < 0) || typeof n !== "number" ){
+        return false
+    }
+
+    const sqrt = Math.sqrt(n);
+    return Number.isInteger(sqrt)
 }
+
+console.log("is 25 squre-->", isSquare(25))
+console.log("is 99 squre-->", isSquare(99))
