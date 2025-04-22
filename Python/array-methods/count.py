0712ca09a35c796653961a192dd084bf690619a8
Samira <samirahekmati@gmail.com>
2025-04-22 10:54:28 +0100

    counting occurance in Py

---
+++ Python/array-methods/count.py
@@ -0,0 +1,3 @@
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+jakes_votes = votes.count("Jake")
