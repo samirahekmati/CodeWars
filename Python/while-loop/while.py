45bb3124baa8c7f3fe7f090a05f3a96e872fa781
Samira <samirahekmati@gmail.com>
2025-04-26 17:42:06 +0100

    while.py

---
+++ Python/while-loop/while.py
@@ -0,0 +1,8 @@
+print("while loop starting")
+count = 0 
+while count <= 3:
+    print("while loop operating")
+    print("count is currently " + str(count))
+    count += 1
+    print("-----")
+print("while loop ended")
