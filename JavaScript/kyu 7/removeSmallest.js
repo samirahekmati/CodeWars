8fef76bc44ca39d04c9c99446440a3c3807e5ffd
Samira <samirahekmati@gmail.com>
2025-05-17 09:48:26 +0100

    remove smallest with splice

--- JavaScript/kyu 7/removeSmallest.js
+++ JavaScript/kyu 7/removeSmallest.js
@@ -15,4 +15,12 @@ function removeSmallest(numbers) {
     //console.log(removeSmallest([2, 2, 1, 2, 1]))
 
 
-  
+    function removeSmallest2(numbers) {
+        numbers = numbers.slice(0); //	Makes a copy to avoid mutating the input array
+        console.log("numbers: ", numbers) 
+        const min = Math.min(...numbers); //	Finds the smallest number
+        numbers.splice(numbers.indexOf(min), 1); //	Removes the first occurrence of the smallest
+        return numbers;
+      }
+
+      console.log(removeSmallest2([2, 2, 1, 2, 1]))
